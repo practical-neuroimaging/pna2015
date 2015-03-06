b4e52ecb44bb51773d5dca24be9694288d3d1250
Matthew Brett <matthew.brett@gmail.com>
2015-03-05 17:35:36 -0800

    Get global signals from new example image

--- day3/get_global_signals.m
+++ day3/get_global_signals.m
@@ -1,5 +1,5 @@
 % Get SPM global signal estimate for all volumes in bold 4D
-file_volno = spm_select('ExtFPList', pwd, 'bold.nii', inf);
+file_volno = spm_select('ExtFPList', pwd, 'ds107_sub012_t1r2.nii', inf);
 V = spm_vol(file_volno);
 global_signals = ones([length(V), 1]);
 for i = 1:length(V)
