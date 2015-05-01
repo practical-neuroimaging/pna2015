66de4a982b4e0ef43023a22e620d258bfe4088e8
Matthew Brett <matthew.brett@gmail.com>
2015-04-30 19:28:21 -0700

    Add do_realign_reslice fixes

--- day10/spm/do_realign_reslice.m
+++ day10/spm/do_realign_reslice.m
@@ -1,4 +1,4 @@
 % Octave / MATLAB script to do realign / reslice
-file_volno = spm_select('ExtFPList', pwd, 'ds114_sub009_t2r1.nii', inf);
-spm_realign(file_volno)
-spm_reslice(file_volno)
+vols = spm_vol('ds114_sub009_t2r1.nii');
+spm_realign(vols);
+spm_reslice(vols);
